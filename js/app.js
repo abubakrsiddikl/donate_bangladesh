f64ca16223ed3a88a1aaee76c237937a8c9e6214
limon <limonsk026@gmail.com>
2024-09-23 22:33:38 +0600

    all donate now section complete done

--- js/app.js
+++ js/app.js
@@ -14,27 +14,59 @@ function getElementByInnerTextById(id) {
   return donationMainBalanceNumber;
 }
 
-// Global variable
-const accountBalance = getElementByInnerTextById("account-balance");
+// 3. This function will calculation and update balance click by Donate Now Button
+// this function fast parameter for input donate-now-input-1,2,3,4 etc
+// this function second parameter for input donation-main-balance-2,3,4,5, etcs
+function multiple(id1, id2) {
+  const donationAmountInput = donateNowInputById(id1);
+  console.log(donationAmountInput)
 
-// add donate now button event handeler
+  //  input filed validation
+  if (!isNaN(donationAmountInput && donationAmountInput > 0)) {
+    //  Get donation balance
+    const donationMainBalance = getElementByInnerTextById(id2);
+
+    // account balance decriment
+    const accountBalance = getElementByInnerTextById("account-balance");
+    
+    // main balance check
+    if (accountBalance >= donationAmountInput) {
+      // increment donation balance
+      const calculateDonateBlance = donationMainBalance + donationAmountInput;
+      const balanceUpdate = document.getElementById(id2);
+      balanceUpdate.innerText = calculateDonateBlance;
+
+      // decrement donation balance
+      const mainAccountBalance = accountBalance - donationAmountInput;
+      const mainAccountBalanceUpdate =
+        document.getElementById("account-balance");
+      mainAccountBalanceUpdate.innerText = mainAccountBalance;
+    } else {
+      alert("This amount is not in balance");
+    }
+  } else {
+    alert("Please type valid amount");
+  }
+}
+
+// Donate Now 1 button add event lisetener
 const donateNow1 = document.getElementById("donate-now-1");
+// add event
+donateNow1.addEventListener('click', function(){
+  multiple('donate-now-input-1' , 'donation-main-balance-1')
+});
 
-// add event listere
-donateNow1.addEventListener("click", function () {
-  // get donation amount
-  const donationAmountInput = donateNowInputById("donate-now-input-1");
-  console.log(donationAmountInput);
-  // increment donation balance
-  const donationMainBalance = getElementByInnerTextById(
-    "donation-main-balance"
-  );
-  const calculateDonateBlance = donationMainBalance + donationAmountInput;
-  const balanceUpdate = document.getElementById("donation-main-balance");
-  balanceUpdate.innerText = calculateDonateBlance;
 
-  // account balance decriment
-  const mainAccountBalance = accountBalance - donationAmountInput;
-  const mainAccountBalanceUpdate = document.getElementById("account-balance");
-  mainAccountBalanceUpdate.innerText = mainAccountBalance;
+// Donate Now 2 button add event lisetener
+const donateNow2 = document.getElementById('donate-now-2');
+// add evnt
+donateNow2.addEventListener('click', function(){
+  multiple('donate-now-input-2' , 'donation-main-balance-2')
+})
+
+// Donate Now 3 button add event lisetener
+const donateNow3 = document.getElementById('donate-now-3');
+// add event 
+donateNow3.addEventListener('click' ,function(){
+  multiple('donate-now-input-3' , 'donation-main-balance-3')
 });
